a37eb63b9c9cd17d1e4fded33a6be6d6c3ef98fa
Jerome Valette <jerome.valette@mypangee.com>
2025-06-08 17:49:20 +0200

    feat: subtask modal edit, actions column, compact icon buttons

---
+++ TaskManager-Angular/src/app/features/tasks/edit-subtask-dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { Inject } from '@angular/core';
+import { ISubtask } from './subtask.model';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-edit-subtask-dialog',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatDialogModule, MatButtonModule],
+  templateUrl: './edit-subtask-dialog.component.html',
+  styleUrls: ['./edit-subtask-dialog.component.scss']
+})
+export class EditSubtaskDialogComponent {
+  private fb = inject(FormBuilder);
+  private dialogRef = inject(MatDialogRef<EditSubtaskDialogComponent>);
+  subtaskForm: FormGroup;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Partial<ISubtask>) {
+    this.subtaskForm = this.fb.group({
+      id: [data.id ?? null],
+      title: [data.title ?? '', Validators.required],
+      description: [data.description ?? ''],
+      status: [data.status ?? 'pending', Validators.required]
+    });
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  onSave() {
+    if (this.subtaskForm.valid) {
+      this.dialogRef.close(this.subtaskForm.value);
+    } else {
+      this.subtaskForm.markAllAsTouched();
+    }
+  }
+} 
