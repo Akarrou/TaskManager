cb90cc18ff322dfc1ace44451c4bcb231d08574e
Jerome Valette <jerome.valette@mypangee.com>
2025-06-30 10:18:23 +0200

    fix: restore accordion design and apply proper app styling

--- TaskManager-Angular/src/app/features/epic-kanban/components/search-filters/search-filters.component.ts
+++ TaskManager-Angular/src/app/features/epic-kanban/components/search-filters/search-filters.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnInit, signal, computed } from '@angular/core';
+import { Component, inject, OnInit, signal, computed, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
@@ -32,12 +33,13 @@ export interface KanbanSearchFilters {
     CommonModule, 
     MatIconModule,
     MatChipsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule
   ],
   templateUrl: './search-filters.component.html',
   styleUrls: ['./search-filters.component.scss']
 })
-export class SearchFiltersComponent implements OnInit {
+export class SearchFiltersComponent implements OnInit, OnDestroy {
   private store = inject(Store);
   private userService = inject(UserService);
   private destroy$ = new Subject<void>();
